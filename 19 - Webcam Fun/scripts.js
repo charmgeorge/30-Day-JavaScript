e84d2e4ed1e1d6b3036a5e3264c7a066ddee015f
charmgeorge <charmcgeorge@yahoo.com>
2017-11-01 20:57:04 -0700

    adds red effect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,8 +23,14 @@ function paintToCanvas() {
   canvas.width = width;
   canvas.height = height;
 
-  setInterval(() =>{
-    ctx.drawImage(video, 0, 0, width, height)
+  return setInterval(() =>{
+    ctx.drawImage(video, 0, 0, width, height);
+    //take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //messes with pixels
+    pixels = redEffect(pixels);
+    //puts the pixels back 
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -35,7 +41,20 @@ function takePhoto(){
 
   //take the data out of the canvas
   const data = canvas.toDataURL('image/jpeg');
-  console.log(data);
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'coolio');
+  link.innerHTML = `<img src="${data}" alt="cool picture" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
 }
 
 getVideo();
