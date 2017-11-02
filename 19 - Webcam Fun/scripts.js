7e16cea045e4b08dae9665a7000b68950c846955
charmgeorge <charmcgeorge@yahoo.com>
2017-11-01 20:08:50 -0700

    adds shutter sound when taking a pic

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,42 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+    })
+    .catch(err =>{
+      console.error(`OH NO!!`, err);
+    })
+  }
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() =>{
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 16);
+}
+
+function takePhoto(){
+  //plays the shutter sound when picture is taken
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  console.log(data);
+}
+
+getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas);
